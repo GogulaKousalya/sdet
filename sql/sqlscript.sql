e533372e9aa3902dc755653bc26cec117961df39
GogulaKousalya <76985862+GogulaKousalya@users.noreply.github.com>
2021-01-18 12:48:02 +0530

    Add files via upload

---
+++ sql/sqlscript.sql
@@ -0,0 +1,68 @@
+REM   Script: Activity2
+REM   Activity2
+
+CREATE TABLE tableName;
+
+CREATE TABLE tableName;
+
+CREATE TABLE tableName1;
+
+CREATE TABLE salesman1;
+
+CREATE TABLE salesman1;
+
+CREATE TABLE salesman1;
+
+CREATE TABLE salesman1;
+
+Create TABLE salesman1;
+
+Create TABLE salesman1;
+
+Create TABLE salesman1;
+
+Create TABLE salesman1;
+
+Create TABLE salesman1;
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman1 ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+CREATE TABLE salesman1 ( 
+    salesman_id int, 
+    salesman_name varchar2(32), 
+    salesman_city varchar2(32), 
+    commission int 
+);
+
+SELECT * FROM salesman1;
+
+INSERT INTO salesman VALUES(1, 'preethi', 'knr', 13);
+
+INSERT ALL 
+    INTO salesman1 VALUES(2, 'sree', 'knr', 11) 
+    INTO salesman1 VALUES(3, 'laxmi', 'hyd', 14) 
+    INTO salesman1 VALUES(4, 'ravali', 'pdpl', 13) 
+    INTO salesman1 VALUES(5, 'jyothi', 'us', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman1;
+
