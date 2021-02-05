9f89b5de822c90d0f854c957673c7b52c7b0877e
GogulaKousalya <76985862+GogulaKousalya@users.noreply.github.com>
2021-02-05 10:04:33 +0530

    Add files via upload

---
+++ java/Exceptionhandling.java
@@ -0,0 +1,34 @@
+package JavaActivity3;
+
+class CustomException extends Exception {
+    private String message = null;
+
+    public CustomException(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
+
+public class Exceptionhandling {
+    public static void main(String[] a){
+        try {
+            Exceptionhandling.exceptionTest("Will print to console");
+            Exceptionhandling.exceptionTest(null);
+            Exceptionhandling.exceptionTest("Won't execute");
+        } catch(CustomException mae) {
+            System.out.println("Inside catch block: " + mae.getMessage());
+        }
+    }
+
+    static void exceptionTest(String str) throws CustomException {
+        if(str == null) {
+            throw new CustomException("String value is null");
+        } else {
+            System.out.println(str);
+        }
+    }
+}
